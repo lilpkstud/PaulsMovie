a65cc7aa95271d168b560793a5b15e4859c63257
lilpkstud <lilpkstud@hotmail.com>
2019-01-08 15:34:34 -0800

    including index and tmdb files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    /*Created my own DB but try to use TMBD API"
+    require('connection.php'); 
+    */
+    require('tmdb.php');
+    include 'ChromePhp.php';
+
+
+    //ChromePhp::log('Hello console!');
+    ChromePhp::log($_SERVER);
+    //ChromePhp::warn('something went wrong!');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Movie Recommendations</h1>
+</body>
+</html>
